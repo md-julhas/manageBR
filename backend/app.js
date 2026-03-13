008ed1287b9e161683392cd769a71e87c9d34bd1
Md Julhas <julhas.info@gmail.com>
2026-03-13 10:57:28 +0600

    refactor(cors): simplify CORS configuration by allowing all origins

--- backend/app.js
+++ backend/app.js
@@ -25,13 +25,13 @@ const limiter = rateLimit({
 
 app.use(limiter)
 
-// app.use(cors())
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-)
+app.use(cors())
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//   })
+// )
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(helmet())
